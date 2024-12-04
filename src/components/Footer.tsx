f546f888c428fff26efd0a0e07b0ed9a2635eeaf
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2024-12-05 04:58:17 +0900

    feat: add white paper and tokenmics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,15 @@ const Footer: React.FC = () => {
     <footer className="py-6 bg-gray-800 text-white">
       <div className="container mx-auto text-center">
         <p>
+          <a href="https://github.com/DeReview/resources/blob/main/whitepaper.pdf" target="_blank" className="underline hover:text-gray-300">
+            Whitepaper
+          </a>{' '}
+          |{' '}
+          <a href="https://github.com/DeReview/resources/blob/main/tokenomics.pdf" target="_blank" className="underline hover:text-gray-300">
+            Tokenomics
+          </a>
+        </p>
+        <p className='mt-4'>
           © 2024 DeReview |{' '}
           <Link to="/privacy-policy" className="underline hover:text-gray-300">
             Privacy Policy
